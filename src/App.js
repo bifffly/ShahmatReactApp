7cbe116f3cc5c0b37efcc6b9781aebf4905e5f5e
Bryce Colson <bryzcolson@gmail.com>
2023-11-02 22:13:41 -0400

    Added ability to play random bot

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import {useEffect, useState} from "react";
+import { Chess } from "chess.js";
+import { Chessboard } from "react-chessboard";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+  const [game, setGame] = useState(new Chess());
+
+  function move(currMove) {
+    const gameCopy = new Chess(game.fen());
+    const result = gameCopy.move(currMove);
+    setGame(gameCopy);
+    return result;
+  }
+
+  function moveRandomly() {
+    const possibleMoves = game.moves();
+    const numPossibleMoves = possibleMoves.length;
+    if (game.isGameOver() || game.isDraw() || numPossibleMoves === 0) {
+      return;
+    }
+    const randomIdx = Math.floor(Math.random() * numPossibleMoves);
+    const randomMove = possibleMoves[randomIdx];
+    move(randomMove);
+  }
+
+  function onDrop(src, trg) {
+    const currMove = move({
+      from: src,
+      to: trg,
+      promotion: "q",
+    });
+    return currMove !== null;
+  }
+
+  useEffect(() => {
+    if (game.turn() === 'b') {
+      setTimeout(moveRandomly, 1000);
+    }
+  })
+
+  return <Chessboard boardWidth={400} position={game.fen()} onPieceDrop={onDrop} />;
 }
-
-export default App;
